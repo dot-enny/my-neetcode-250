9f33f2606d3ff4a004649ba320577979781f37cc
Enny <109947938+dot-enny@users.noreply.github.com>
2025-10-09 10:03:38 +0100

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/version/python/49-GroupAnagrams_20251009_100339.py
@@ -0,0 +1,16 @@
+# Last updated: 10/9/2025, 10:03:39 AM
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+            
+        return list(res.values())
+        
